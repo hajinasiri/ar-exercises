0684d350c38aeb786a38a8fa2155e437cec9e93f
hajinasiri <hajinasiri@gmail.com>
2017-08-29 21:05:55 +0000

    exercise4 is done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -35,7 +35,3 @@ end
 puts "There are #{Store.count} stores in stores table"
 
 
-
-# Burnaby (annual_revenue of 300000, carries men's and women's apparel)
-# Richmond (annual_revenue of 1260000 carries women's apparel only)
-# Gastown (annual_revenue of 190000 carries men's apparel only)
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,40 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+surrey = Store.new do |s|
+  s.name = "Surrey"
+  s.annual_revenue = 224000
+  s.mens_apparel = false
+  s.womens_apparel = true
+  s.save
+end
+
+whistler = Store.new do |s|
+  s.name = "Whistler"
+  s.annual_revenue = 1900000
+  s.mens_apparel = true
+  s.womens_apparel = false
+  s.save
+end
+
+yaletown = Store.new do |s|
+  s.name = "Yaletown"
+  s.annual_revenue = 430000
+  s.mens_apparel = true
+  s.womens_apparel = true
+  s.save
+end
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each{|s|
+puts "#{s.name}  #{s.annual_revenue}"
+}
+
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+
+@womens_stores.each{|s|
+puts "#{s.name} #{s.annual_revenue}"
+}
+
+
